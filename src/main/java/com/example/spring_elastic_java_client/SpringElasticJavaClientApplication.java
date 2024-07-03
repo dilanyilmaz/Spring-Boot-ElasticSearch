11ab4a75b17a39e44ee3c6655fbcd06cfb323651
dilan.yilmaz <dilan.yilmaz@vbt.com.tr>
2024-07-03 15:03:18 +0300

    first commit

---
+++ src/main/java/com/example/spring_elastic_java_client/SpringElasticJavaClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_elastic_java_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringElasticJavaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringElasticJavaClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_elastic_java_client/SpringElasticJavaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_elastic_java_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringElasticJavaClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
